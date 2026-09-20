90c158ee2654c008e98f09cd117cb57c85f8dd80
Tayyab Ahmed <tayyabahmed7561@gmail.com>
2026-09-20 19:50:10 +0500

    changes in route

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin:"https://besocial-platform.vercel.app",
+    origin: process.env.CLIENT_URL,
     origin:"http://localhost:5173",
     credentials:true
 }))
